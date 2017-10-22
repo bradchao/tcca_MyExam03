54641e1eed11bdf9198e7dc3ea35a4c034e8f34f
Brad Chao <brad@brad.tw>
2017-10-22 12:12:01 +0800

    2017-10-22V3

--- app/src/main/java/com/example/administrator/myexam03/MainActivity.java
+++ app/src/main/java/com/example/administrator/myexam03/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.administrator.myexam03;
 
 import android.app.ListActivity;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
@@ -17,12 +19,15 @@ public class MainActivity extends ListActivity {
     private LinkedList<HashMap<String,String>> data;
     private String[] from = {"name","address"};
     private int[] to = {R.id.name, R.id.address};
+    private UIHandler uiHandler;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
+
+        uiHandler = new UIHandler();
 
         listView = getListView();
 
@@ -46,13 +51,21 @@ public class MainActivity extends ListActivity {
     private void parseJSON(String json){
 
 
-        initListView();
+        uiHandler.sendEmptyMessage(0);
     }
 
     private void initListView(){
         adapter = new SimpleAdapter(
                 this,data, R.layout.item, from, to);
         listView.setAdapter(adapter);
+    }
+
+    private class UIHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            initListView();
+        }
     }
 
 }

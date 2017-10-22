365d9706ca34726cb0fd5135a0ad9c5995f0fa38
Brad Chao <brad@brad.tw>
2017-10-22 13:32:11 +0800

    2017-10-22V4

--- app/src/main/java/com/example/administrator/myexam03/MainActivity.java
+++ app/src/main/java/com/example/administrator/myexam03/MainActivity.java
@@ -5,9 +5,18 @@ import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -41,17 +50,54 @@ public class MainActivity extends ListActivity {
         new Thread(){
             @Override
             public void run() {
+                try {
+                    URL url = new URL("http://data.coa.gov.tw/Service/OpenData/ODwsv/ODwsvTravelFood.aspx");
+                    HttpURLConnection conn =
+                            (HttpURLConnection) url.openConnection();
+                    conn.connect();
 
-                parseJSON("json");
+                    BufferedReader reader =
+                            new BufferedReader(
+                                    new InputStreamReader(conn.getInputStream()));
+
+                    StringBuffer sb = new StringBuffer();
+                    String line = null;
+                    while ( (line = reader.readLine()) != null){
+                        sb.append(line);
+                    }
+
+                    reader.close();
+                    //Log.i("brad", sb.toString());
+                    parseJSON(sb.toString());
+                }catch(Exception e){
+                    Log.i("brad", e.toString());
+                }
             }
         }.start();
     }
 
 
     private void parseJSON(String json){
+        try {
+            data = new LinkedList<>();
+            JSONArray root = new JSONArray(json);
+            for (int i=0; i<root.length(); i++){
+                JSONObject obj = root.getJSONObject(i);
 
+                HashMap<String,String> row = new HashMap<>();
+                row.put(from[0], obj.getString("Name"));
+                row.put(from[1], obj.getString("Address"));
+                row.put("tel", obj.getString("Tel"));
+                row.put("imgfile", obj.getString("PicURL"));
 
-        uiHandler.sendEmptyMessage(0);
+                data.add(row);
+
+            }
+
+            uiHandler.sendEmptyMessage(0);
+        }catch(Exception e){
+
+        }
     }
 
     private void initListView(){
